9e48f821ab32424c155f6ebda789dbd2d907077c
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-06-09 15:28:11 -0300

    req 2 e 3 passando local

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,32 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    first_memory = {}
+    second_memory = {}
+    for index, first in enumerate(first_string):
+        try:
+            first_memory[first] += 1
+        except KeyError:
+            first_memory[first] = 1
+        
+        try:
+            second_memory[second_string[index]] += 1
+        except KeyError:
+            second_memory[second_string[index]] = 1
+    for i in first_memory.keys():
+        try:
+            if first_memory[i] != second_memory[i]:
+                return False
+        except KeyError:
+            return False
+    return True
+
+# var1 = 'coxinha'
+# var2 = 'empada'
+# print(is_anagram(var1, var2))
+# m = {}
+# m[1] = 1
+# print(len(var1))
+# for ola in enumerate(var1):
+#     print(ola)
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,19 @@
 def is_palindrome_recursive(word, low, high):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    
+    if (high == 0):
+       return True
+
+    if (word[low] != word[high]):
+        # print(low)
+        return False
+
+    if (low < high):
+        return is_palindrome_recursive(word, low + 1, high - 1)
+    # print(low)
+    return True
+
+# var = 'AGUA'
+# print(is_palindrome_recursive(var, 0, len(var) - 1))
