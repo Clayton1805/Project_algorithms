fb6fa0f620c95f3e444b238c58b8ffa348a7cc40
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-06-09 15:37:36 -0300

    lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -9,11 +9,11 @@ def is_anagram(first_string, second_string):
             first_memory[first] += 1
         except KeyError:
             first_memory[first] = 1
-        
         try:
             second_memory[second_string[index]] += 1
         except KeyError:
             second_memory[second_string[index]] = 1
+
     for i in first_memory.keys():
         try:
             if first_memory[i] != second_memory[i]:
@@ -21,12 +21,3 @@ def is_anagram(first_string, second_string):
         except KeyError:
             return False
     return True
-
-# var1 = 'coxinha'
-# var2 = 'empada'
-# print(is_anagram(var1, var2))
-# m = {}
-# m[1] = 1
-# print(len(var1))
-# for ola in enumerate(var1):
-#     print(ola)
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,18 +2,14 @@ def is_palindrome_recursive(word, low, high):
     """ Faça o código aqui. """
     if not word:
         return False
-    
-    if (high == 0):
-       return True
+    if high == 0:
+        return True
 
-    if (word[low] != word[high]):
+    if word[low] != word[high]:
         # print(low)
         return False
 
-    if (low < high):
+    if low < high:
         return is_palindrome_recursive(word, low + 1, high - 1)
     # print(low)
     return True
-
-# var = 'AGUA'
-# print(is_palindrome_recursive(var, 0, len(var) - 1))
